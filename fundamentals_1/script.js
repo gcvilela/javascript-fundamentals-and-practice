420b7ee806b223e36ec2b5efbeae488fc75e1cfc
gcvilela <gustavoccvilela@gmail.com>
2024-02-01 14:24:04 -0300

    commit

---
+++ fundamentals_1/script.js
@@ -0,0 +1,13 @@
+let js = 'amazing';
+
+let fName = 'jonas';
+
+console.log(fName);
+
+const jonasName = `My name is ${fName}`;
+const mLines = `this
+is
+multiple
+lines`;
+console.log(jonasName);
+console.log(mLines);
